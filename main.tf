3be639a7d5b938f6a8fdb3e0b3f5e1c33def0ad0
“vpavan8106” <pavandurgavallapuneni014@gmail.com>
2025-08-30 16:37:33 +0530

    destroy

--- main.tf
+++ main.tf
@@ -2,47 +2,47 @@ provider "aws" {
   region = var.aws_region
 }
 
-#resource "aws_ecs_cluster" "devops_cluster" {
-  name = "devops-ecs-cluster"
-}
+# resource "aws_ecs_cluster" "devops_cluster" {
+#   name = "devops-ecs-cluster"
+# }
 
-resource "aws_ecs_task_definition" "app_task" {
-  family                   = "devops-app-task"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-  execution_role_arn       = var.execution_role_arn
+# resource "aws_ecs_task_definition" "app_task" {
+#   family                   = "devops-app-task"
+#   network_mode             = "awsvpc"
+#   requires_compatibilities = ["FARGATE"]
+#   cpu                      = "256"
+#   memory                   = "512"
+#   execution_role_arn       = var.execution_role_arn
 
-  container_definitions = jsonencode([
-    {
-      name      = "devops-app"
-      image     = var.container_image
-      essential = true
-      portMappings = [
-        {
-          containerPort = 80
-        }
-      ]
-    }
-  ])
-}
+#   container_definitions = jsonencode([
+#     {
+#       name      = "devops-app"
+#       image     = var.container_image
+#       essential = true
+#       portMappings = [
+#         {
+#           containerPort = 80
+#         }
+#       ]
+#     }
+#   ])
+# }
 
-resource "aws_ecs_service" "app_service" {
-  name            = "devops-app-service"
-  cluster         = aws_ecs_cluster.devops_cluster.id
-  task_definition = aws_ecs_task_definition.app_task.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
+# resource "aws_ecs_service" "app_service" {
+#   name            = "devops-app-service"
+#   cluster         = aws_ecs_cluster.devops_cluster.id
+#   task_definition = aws_ecs_task_definition.app_task.arn
+#   desired_count   = 1
+#   launch_type     = "FARGATE"
 
-  network_configuration {
-    subnets          = var.subnet_ids
-    security_groups  = [var.security_group_id]
-    assign_public_ip = true
-  }
+#   network_configuration {
+#     subnets          = var.subnet_ids
+#     security_groups  = [var.security_group_id]
+#     assign_public_ip = true
+#   }
 
-  depends_on = [
-    aws_ecs_cluster.devops_cluster,
-    aws_ecs_task_definition.app_task
-  ]
-} #
+#   depends_on = [
+#     aws_ecs_cluster.devops_cluster,
+#     aws_ecs_task_definition.app_task
+#   ]
+# } 
