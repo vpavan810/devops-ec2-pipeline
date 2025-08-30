49c2c53e13fa284306c8fef26380eca7dcbc3db5
“vpavan8106” <pavandurgavallapuneni014@gmail.com>
2025-08-30 15:46:09 +0530

    ec2 to ecs

--- variables.tf
+++ variables.tf
@@ -2,10 +2,20 @@ variable "aws_region" {
   default = "ap-south-1"
 }
 
-variable "ami_id" {
-  default = "ami-0f5ee92e2d63afc18" # Amazon Linux 2 (Mumbai)
+variable "execution_role_arn" {
+  description = "arn:aws:iam::851725564646:role/ecstask"
 }
 
-variable "instance_type" {
-  default = "t2.micro"
+variable "container_image" {
+  default = "nginx" # Or your custom image
+}
+
+variable "subnet_ids" {
+  type = list(string)
+  default = [ "subnet-09f026882a20e5555" ]
+}
+
+variable "security_group_id" {
+  type = string
+  default = "sg-07705b46489ac5a88"
 }
